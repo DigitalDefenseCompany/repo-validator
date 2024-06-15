9408f7f84db9ea50f0f625285558061b73027c49
Saurabh Chalke <saurabhchalke@gmail.com>
2024-06-15 21:28:45 +0000

    feat: add validation for checking if the project if of foundry type

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as fs from 'fs'
+import * as path from 'path'
 
 /**
  * The main function for the action.
@@ -7,20 +8,16 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const repoPath = process.env.GITHUB_WORKSPACE || ''
+    const foundryFilePath = path.join(repoPath, 'foundry.toml')
+    if (!fs.existsSync(foundryFilePath)) {
+      core.setFailed('foundry.toml file is missing in the root directory.')
+      return
+    }
+    core.info('foundry.toml file found in the root directory.')
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
